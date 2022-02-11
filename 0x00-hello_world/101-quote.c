f88aebaf86d5d803fc98a197b81902dd129123b0
taret92 <ivancho92.10@hotmail.com>
2022-02-11 01:33:50 -0500

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n);
+	write(1, "and that piece of art is useful\" ", 35);
+	write(1, "- Dora Korpar, 2015-10-19,\n", 29);
 
-	return:(1);
-	}
+	return (1);
+}
