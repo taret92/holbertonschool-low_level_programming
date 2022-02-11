74b1dcff67961e3306ff066360b3835017bfe59b
taret92 <ivancho92.10@hotmail.com>
2022-02-10 16:41:41 -0800

    5fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *print with proper grammar, but the outcome is a piece of art
-*/
+ * print with proper grammar, but the outcome is a piece of art
+ */
 
 int main(void)
 {
 	printf:("with proper grammar, but the outcome is a piece of art,");
-
 	return(0);
 }
