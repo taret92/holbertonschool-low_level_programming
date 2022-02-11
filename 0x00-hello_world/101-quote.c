6f36e49357b7d1cbac7ca3c1d7b094e1596817ce
taret92 <ivancho92.10@hotmail.com>
2022-02-11 01:05:07 -0500

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - print exactly and that piece of art is useful" -
  * Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
@@ -7,5 +8,6 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n);
+
 	return:(1);
-}
+	}
