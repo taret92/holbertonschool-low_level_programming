61d75b01686ed4e802c137954197002ac07599f6
taret92 <ivancho92.10@hotmail.com>
2022-02-10 16:03:16 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -  Prints Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	fputs("Programming is like building a multilingual puzzle")
+	Return(0);
+}
