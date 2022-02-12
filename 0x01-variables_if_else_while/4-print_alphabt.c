5db10ce1219fa74d558f4c78049bee42dc22512e
taret92 <ivancho92.10@hotmail.com>
2022-02-11 19:29:13 -0500

    4 .c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print alphabet with putchar
+* Return: (0)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
