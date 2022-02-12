a0378d486a3082d98d138a9bce8de9fa8060a269
taret92 <ivancho92.10@hotmail.com>
2022-02-11 20:51:34 -0500

    comb numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - print combination of numbers
+* Return: 0
+*/
+
+int main(void)
+
+{
+
+int number;
+
+	for (number = 0; number <= 9; number++)
+{
+	putchar(number + '0');
+
+	if (number < 9)
+	{
+
+	putchar(',');
+
+	putchar(' ');
+	}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
