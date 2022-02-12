42bd3e3343b18c09196a7218f0f4859107b157ef
taret92 <ivancho92.10@hotmail.com>
2022-02-11 20:08:04 -0500

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet with putchar
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
