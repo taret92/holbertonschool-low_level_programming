5218df73396493c009a3bcb73de868a4e9669db0
taret92 <ivancho92.10@hotmail.com>
2022-02-15 23:50:30 -0500

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet alphabet in low showcase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char ch = 'a';
+
+	for (; ch <= 'z' ; ch++)
+	
+	{
+
+		_putchar(ch);
+	
+	}
+
+	_putchar('\n');
+
+
+}
