27b49416f0b06afc4e3600f5ae6e2e373cf35f72
taret92 <ivancho92.10@hotmail.com>
2022-02-16 20:31:16 -0500

    0 task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * positive_or_negative - show if a number is positive or negative
+ * @i: int parameter
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	return (0);
+}
