ee6ee4e5ea7e365464f2767c3a81fbee14eb9448
taret92 <ivancho92.10@hotmail.com>
2022-02-20 00:25:42 -0500

    task 5

---
+++ 0x04-more_functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print x10 numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
