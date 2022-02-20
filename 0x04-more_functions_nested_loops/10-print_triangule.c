44758f1562233e436260ed183c5bf913fadbd66d
taret92 <ivancho92.10@hotmail.com>
2022-02-20 18:09:39 -0500

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangule.c
+++ 0x04-more_functions_nested_loops/10-print_triangule.c
@@ -1,20 +1,33 @@
 #include "main.h"
+
 /**
-* print_triangle - prints a triangle
-* @size: takes in a integer 
+* print_triangle- prints triangle of #
+* @size: size of the triangle
+* Return: none
 */
+
 void print_triangle(int size)
 {
-	int row, column, k;
+
+	int a, b, c;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (row = 0; row < size; row++)
 	{
-		for (column = size - row; column > 1; column--)
-			_putchar(' ');
-		for (k = row + column; k >= 1; k--)
-			_putchar('#');
-		_putchar('\n');
+	_putchar('\n');
 	}
-}
+	else
+	{
+	for (a = 0; a < size; a++)
+	{
+	for (b = size - a; b > 1; b--)
+	{
+	_putchar(' ');
+	}
+	for (c = 0; c <= a; c++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+	}
