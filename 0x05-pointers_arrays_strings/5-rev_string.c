d95d84c944663ad7911d020287232482a3b5fb02
taret92 <ivancho92.10@hotmail.com>
2022-02-21 23:17:12 -0500

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * rev_string - reverse the string
+ * @s: pt
+ * Return: 0
+ */
+
+void rev_string(char *s)
+
+{
+int a, l, t;
+
+	l = strlen(s);
+
+	for (a = 0; a < l / 2; a++)
+
+	{
+		t = s[a];
+		s[a] = s[l - a - l];
+		s[l - a - l] = t;
+	}
+
+}
