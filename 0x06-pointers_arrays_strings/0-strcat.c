5311df4c279630954adb21a04d968dddc17b9479
taret92 <ivancho92.10@hotmail.com>
2022-02-24 22:18:32 -0500

    first commit dir 6

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: dest string
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+
+while (dest[i] != ('\0'))
+	i++;
+
+while (src[j] != ('\0'))
+
+	j++;
+{
+dest[i] = src[j];
+	i++;
+	j++;
+}
+dest[i] = '\0';
+return (dest);
+}
