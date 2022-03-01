a3aeff9b3b980880225450c79dd70eb6544cefb8
taret92 <ivancho92.10@hotmail.com>
2022-02-28 22:42:57 -0500

    task 3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -33,4 +33,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
 #endif /* MAIN_H */
