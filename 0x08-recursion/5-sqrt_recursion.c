aaf56620fe8dbd2f624e0bc1e7c1ae89908b40ec
taret92 <ivancho92.10@hotmail.com>
2022-03-02 21:43:16 -0500

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find square
+ * @n: given number
+ * Return: square
+ */
+
+int _sqrt_recursion(int n)
+
+{
+	int i;
+
+	if (n < 0 || n == 1)
+		return (-1);
+
+	if (i < n || i * i == n)
+
+	return (n * _sqrt_recursion(n + 1));
+
+
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -41,5 +41,6 @@ void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 #endif /* MAIN_H */
 
