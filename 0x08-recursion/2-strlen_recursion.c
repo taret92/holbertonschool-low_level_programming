302def32ca851e70e4d0ca08d71674541f90a0be
taret92 <ivancho92.10@hotmail.com>
2022-03-02 19:11:57 -0500

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _strlen_recursion - return total bytes
+ * @s: string
+ * Return: length
+ */
 
 int _strlen_recursion(char *s)
 
@@ -7,7 +11,7 @@ int _strlen_recursion(char *s)
 	if (!s || !*s)
 		return (0);
 
-	return 1 +(_strlen_recursion (s + 1));
+	return (1 + _strlen_recursion(s + 1));
 
 
 }
