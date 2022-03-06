eca165aca7c7e61193e9e34f178f5513e2471a3b
taret92 <ivancho92.10@hotmail.com>
2022-03-06 13:48:04 -0500

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - maing function
+ * @argc: number of objects
+ * @argv: array of content
+ * @mul: contain the multiplication
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+	int res;
+
+	for (i = 0; i < argc; i++)
+		;
+	if (i < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+		return (0);
+	}
+}
