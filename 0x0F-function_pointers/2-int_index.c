f35f24ed0c19d77d361615e9e62e8c58a4c5897c
taret92 <ivancho92.10@hotmail.com>
2022-03-17 23:05:49 -0500

    task 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,21 +1,19 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function that executes a
- * function given as a parameter on each
- * element of an array
- * @array: Pointer to array
- * @size: number of elements
- * @action: pointer to function
- * Return: Nothing
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
+ *
+ * Return: no return.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
-	if (array != NULL && size > 0 && action != NULL)
-	{
+	if (array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
-	}
 }
