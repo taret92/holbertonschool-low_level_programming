a769fcab018a58483b56d080eb4262194c12970d
taret92 <ivancho92.10@hotmail.com>
2022-03-29 22:43:14 -0500

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,18 @@
 #include "lists.h"
-
-
+/**
+ * list_len count number of elements
+ * @h: singly linked list
+ * @return number elements
+ */
 
 size_t list_len(const list_t *h)
+{
+size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
