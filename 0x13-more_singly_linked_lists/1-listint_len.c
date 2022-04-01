77598f5ac422bb2c5bb8e0902322907ecfadd166
taret92 <ivancho92.10@hotmail.com>
2022-03-31 21:58:34 -0500

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Return the number of elements in a linked
+ * list
+ * @h: head of list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t nodos = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodos++;
+	}
+	return (nodos);
+}
