aaee6c6c3c1bc88ecdcbe29b1bce007839945d77
taret92 <ivancho92.10@hotmail.com>
2022-04-01 21:56:07 -0500

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head: head of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while ((temp = *head) != NULL)
+	{
+		*head = (*head)->next;
+		free(temp);
+	}
+}
