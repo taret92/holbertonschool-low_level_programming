c6401140efc3753d2ffa39eccc2b0f1c59ca1839
taret92 <ivancho92.10@hotmail.com>
2022-04-03 18:53:18 -0500

    task 6 fixed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,22 +8,22 @@
 int pop_listint(listint_t **head)
 {
 
-int i;
-listint_t *temp1;
-listint_t *temp2;
+	int i;
+	listint_t *temp1;
+	listint_t *temp2;
 
 	if (*head == NULL)
 		return (0);
 
-	temp1 = *head;
+	temp2 = *head;
 
-	i = temp1->n;
+	i = temp2->n;
 
-	temp2 = temp1->next;
+	temp1 = temp2->next;
 
 	free(temp2);
 
-	*head = temp2;
+	*head = temp1;
 
 	return (i);
 }
