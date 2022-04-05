4fc17420e9d6f0e64eba3178beaff0c018c8a367
taret92 <ivancho92.10@hotmail.com>
2022-04-05 13:35:05 -0500

    task 1 fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <stddef.h>
+#include "main.h"
 
 /**
  * print_binary - print a number in binary
  * @n: number
+ * Return: binary
  */
 
 void print_binary(unsigned long int n)
 {
-
 		if (n >> 1)
 			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
