5106d7367728916cd520d85c48d5f008025cc80e
taret92 <ivancho92.10@hotmail.com>
2022-04-05 01:28:20 -0500

    task 0 fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,26 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 	int i;
-	{
-	if (b != 0)
+
+	if (b == 0)
 	return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+
+		return (0);
+
 	}
 
 	for (i = 0; b[i]; i++)
 	{
 		num <<= 1;
-		if (b[i] == 1)
+		if (b[i] == '1')
 
+		{
 			num += 1;
+		}
 	}
 	return (num);
 }
