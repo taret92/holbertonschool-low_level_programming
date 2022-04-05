ef64bfe0078eff3f0494bf13b350170bd5e581a5
taret92 <ivancho92.10@hotmail.com>
2022-04-05 01:14:15 -0500

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i;
 	{
-	if (b == 0)
+	if (b != 0)
 	return (0);
-	}
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-		return (0);
-		}
 	}
 
 	for (i = 0; b[i]; i++)
@@ -27,9 +20,7 @@ unsigned int binary_to_uint(const char *b)
 		num <<= 1;
 		if (b[i] == 1)
 
-		{
 			num += 1;
-		}
 	}
-	return(num);
+	return (num);
 }
