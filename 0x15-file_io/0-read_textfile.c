90572aa471028e6fc2d0bc0e4fb5f1fa8bca296d
taret92 <ivancho92.10@hotmail.com>
2022-04-06 12:42:16 -0500

    Readme.md and task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile - read a text and print the letters
+ * @filename: file name
+ * @letters: number of letters
+ * Return: wr
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t rd, wr;
+	int fd;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	close(fd);
+
+	free(buf);
+
+	return (wr);
+}
