28f78d07bca3215cb6319ca114f017784a12f77d
taret92 <ivancho92.10@hotmail.com>
2022-04-06 13:00:41 -0500

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	rd = read(fd, buf, letters);
 	wr = write(STDOUT_FILENO, buf, rd);
-	if (wr == -1 || wr != read)
+	if (wr == -1 || wr != rd)
 
 		return (0);
 
