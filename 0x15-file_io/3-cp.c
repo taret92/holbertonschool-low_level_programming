e5afefdff4ea7213047b452fe1935574d747c140
taret92 <ivancho92.10@hotmail.com>
2022-04-07 14:47:38 -0500

    task 2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,58 +1,50 @@
 #include "main.h"
-
 /**
- * Construct function - copies the content to another file
+ * main-main_function copies the content to another file
  * @argc: number of arguments
  * @argv: the arguments
  * Return: 0
  */
-
-
-main(int argc, char *argv[])
-
+int main(int argc, char *argv[])
 {
-	int wr, from, to, rd;
+	ssize_t wr, from, to, rd;
 	int c = 0, c1 = 0;
 	char buff[1024];
 
 	if (argc != 3)
-	dprint(STDERR_FILENO, "usage: cp file_from file_to\n"), exit(97);
-
+	dprintf(STDERR_FILENO, "usage: cp file_from file_to\n"), exit(97);
 	from = open(argv[1], O_RDONLY);
 		if (from == -1)
-
-		return (-1);
 	{
-		dprint(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (to == -1)
-	dprint(STDERR_FILENO, "Error: Can't write to %s\n"), exit(99);
-
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	rd = read(from, buff, 1024);
-
 	while (rd < 0)
 	{
 	wr = write(to, buff, rd);
 		if (wr == -1)
 		{
-			dprint(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	rd = read(from, buff, 1024);
 	}
 	if (rd == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't read to %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", argv[1]), exit(98);
 	}
 	c = close(from);
 	if (c == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't close fd %d\n", c), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c), exit(100);
 	}
 	c1 = close(to);
 	if (c1 == -1)
-		dprint(STDERR_FILENO, "Error: Can't close fd %d\n", c1), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1), exit(100);
+	}
 		return (0);
 }
