9c100b6e7efb90673890be3e698f32baa34522bf
taret92 <ivancho92.10@hotmail.com>
2022-04-06 22:28:15 -0500

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		return(-1);
+	if (text_content)
+	{
 
 	for (numletters = 0; text_content[numletters]; numletters++)
 		;
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (rwr == -1)
 		return (-1);
-
+	}
 	close(fd);
 
 	return (1);
