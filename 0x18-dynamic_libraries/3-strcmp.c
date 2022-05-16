65c2c60b99504f3e4d09d036e08d32d6b00d070a
taret92 <ivancho92.10@hotmail.com>
2022-05-16 18:41:08 -0500

    task 0 and Readme.md

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+
+{
+int i = 0;
+int a = 0;
+
+while (s1[i] == s2[i])
+	{
+	if (s1[i] == '\0')
+		return (0);
+	i++;
+	}
+a = (s1[i] - s2[i]);
+return (a);
+
+}
