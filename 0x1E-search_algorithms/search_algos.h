4d25f1184c1707847cd6558bce9e91f5044114b5
taret92 <ivancho92.10@hotmail.com>
2022-08-11 23:17:24 -0500

    task 101

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,34 @@
 #ifndef __search_algos_H__
 #define __search_algos_H__
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <math.h>
+#include <stdlib.h>
+
+void print_array(int *array, size_t inc, size_t size);
+
+typedef struct skiplist_s
+{
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
+
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-void print_array(int *array, size_t inc, size_t size);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
