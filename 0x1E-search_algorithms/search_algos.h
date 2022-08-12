59906faec9eb09c1e7096d5289795c883c4b0b54
taret92 <ivancho92.10@hotmail.com>
2022-08-11 23:00:01 -0500

    task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-
+void print_array(int *array, size_t inc, size_t size);
 #endif
